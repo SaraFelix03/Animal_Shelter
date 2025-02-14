59bbf49d238f6efd33da4192b96dd52ee54801ea
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-02-14 18:47:16 +0100

    Implemantation of VeterinaryVisit and MedicalHistoryManager

--- src/main/java/it/unimol/app/VeterinaryVisit.java
+++ src/main/java/it/unimol/app/VeterinaryVisit.java
@@ -1,6 +1,74 @@
 package it.unimol.app;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class VeterinaryVisit implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private LocalDate dateOfVisit;
+    private String veterinaryName;
+    private String diagnosis;
+    private String prescriptions;
+    private String cost;
+
+    public VeterinaryVisit(LocalDate dateOfVisit, String veterinaryName,
+                           String diagnosis, String prescriptions, String cost) {
+        this.dateOfVisit = dateOfVisit;
+        this.veterinaryName = veterinaryName;
+        this.diagnosis = diagnosis;
+        this.prescriptions = prescriptions;
+        this.cost = cost;
+    }
+
+    public LocalDate getDateOfVisit() {
+        return dateOfVisit;
+    }
+
+    public void setDateOfVisit(LocalDate dateOfVisit) {
+        this.dateOfVisit = dateOfVisit;
+    }
+
+    public String getVeterinaryName() {
+        return veterinaryName;
+    }
+
+    public void setVeterinaryName(String veterinaryName) {
+        this.veterinaryName = veterinaryName;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void setPrescriptions(String prescriptions) {
+        this.prescriptions = prescriptions;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "VeterinaryVisit{" +
+                "dateOfVisit=" + dateOfVisit +
+                ", veterinaryName='" + veterinaryName + '\'' +
+                ", diagnosis='" + diagnosis + '\'' +
+                ", prescriptions='" + prescriptions + '\'' +
+                ", cost='" + cost + '\'' +
+                '}';
+    }
 }
--- src/main/java/it/unimol/app/managers/MedicalHistoryManager.java
+++ src/main/java/it/unimol/app/managers/MedicalHistoryManager.java
@@ -1,26 +1,41 @@
 package it.unimol.app.managers;
 
+import it.unimol.app.Animal;
 import it.unimol.app.VeterinaryVisit;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MedicalHistoryManager implements Serializable {
     private static final long serialVersionUID = 1L;
+
     private static MedicalHistoryManager instance;
     private static Map<Integer, List<VeterinaryVisit>> visitsHistory;
 
     private MedicalHistoryManager() {
-        visitsHistory = new HashMap();
+        visitsHistory = new HashMap<Integer, List<VeterinaryVisit>>();
     }
 
     public static MedicalHistoryManager getInstance() {
         if (instance == null) {
             instance = new MedicalHistoryManager();
         }
-
         return instance;
     }
+
+    public void registerNewVisit(Animal animal, VeterinaryVisit visit) {
+        List<VeterinaryVisit> visits;
+        if(visitsHistory.containsKey(animal.getID())){
+            visits = visitsHistory.get(animal.getID());
+        }else{
+            visits = new ArrayList<>(20);
+        }
+        visits.add(visit);
+        visitsHistory.put(animal.getID(), visits);
+    }
+
+
 }
